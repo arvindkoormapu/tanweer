f18c224d8582c2c5ca1102eab545d3a0e687b35b
Arvind Koormapu <arvindkoormapu@Arvinds-MacBook-Pro.local>
2024-08-08 15:31:19 +0530

    loading page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Loading from './pages/loading';
 import Home from './pages/home';
-import './App.less';
+import './App.css';
 
 const App = () => {
   return (
