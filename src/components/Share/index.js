5a12f5f28d9c9dc4a865450c213823c4fe3d197d
Arvind Koormapu <arvindkoormapu@Arvinds-MacBook-Pro.local>
2024-08-27 10:49:22 +0530

    content changes

--- src/components/Share/index.js
+++ src/components/Share/index.js
@@ -15,43 +15,23 @@ const ShareContent = ({ url, title, imageUrl }) => {
     message.info("Linked Copied!");
   };
 
-  const encodedMessage = encodeURIComponent(`${title} - ${"description"}`);
-  const encodedUrl = encodeURIComponent(imageUrl);
-  const whatsappUrl = `https://wa.me/?text=${encodedMessage}%20${encodedUrl}`;
-
   return (
     <div style={{ display: "flex", justifyContent: "space-around" }}>
-      {/* <Helmet>
+      <Helmet>
         <meta property="og:title" content={title} />
         <meta property="og:description" content="Amazing content to share!" />
         <meta property="og:image" content={imageUrl} />
-        <meta property="og:url" content={url} />
+        <meta property="og:url" content={imageUrl} />
         <meta property="og:type" content="website" />
       </Helmet>
       <TwitterShareButton url={url} title={title}>
         <XIcon size={32} round />
       </TwitterShareButton>
-      <WhatsappShareButton url={url} title={title} separator=":: ">
+      <WhatsappShareButton url={imageUrl} title={title} separator=":: ">
         <WhatsappIcon size={32} round />
       </WhatsappShareButton>
       <div>
         <Button type="primary" icon={<CopyOutlined />} onClick={copyLink} />
-      </div> */}
-      <Helmet>
-        <title>{title}</title>
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={"description"} />
-        <meta property="og:image" content={imageUrl} />
-        <meta property="og:url" content={imageUrl} />
-        <meta property="og:type" content="website" />
-      </Helmet>
-      <div>
-        {/* <h1>{title}</h1>
-        <p>{description}</p>
-        <img src={imageUrl} alt="Preview" /> */}
-        <a href={whatsappUrl} target="_blank" rel="noopener noreferrer">
-          Share on WhatsApp
-        </a>
       </div>
     </div>
   );
