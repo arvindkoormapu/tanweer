5fb8228788bf7cdc839cbba6084458fee395c90d
Arvind <apple@Arvind.local>
2025-02-10 14:34:16 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Loading from "./pages/loading";
 import Landing from "./pages/Landing";
 
