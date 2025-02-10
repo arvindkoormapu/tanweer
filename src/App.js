dbc04a3e4a9db89034cd3514a1b6e5481191af5d
Arvind <apple@Arvind.local>
2025-02-10 11:42:32 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Loading from "./pages/loading";
 import Landing from "./pages/Landing";
 
@@ -24,7 +24,7 @@ import "./App.css";
 
 const App = () => {
   return (
-    <Router basename="/2024">
+    <Router>
       <PageTracker />
       <Routes>
         <Route
